1a0004a9b2b6fef9219db90ae04a43eefa90759a
Novo1999 <neptune.navo@gmail.com>
2025-07-25 13:12:45 +0600

    added: dockerize the app

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     domains: ['cdn.10minuteschool.com', 's3.ap-southeast-1.amazonaws.com'],
   },
